dad96548492e18b8b2d7fb95c17bf21ba37418b1
phpwork512 <phpwork512@yandex.ru>
2023-01-19 17:09:35 +0400

    sprint 13 add-controllers step#1

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
@@ -14,10 +13,14 @@ public class UserDto {
      */
     private int id;
 
-    /** имя пользователя */
+    /**
+     * имя пользователя
+     */
     private String name;
 
-    /** email пользователя */
+    /**
+     * email пользователя
+     */
     @Email(message = "Электронная почта указана неверно")
     private String email;
 }

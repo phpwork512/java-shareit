fb0df38466f20f089a450290f809b004c4ac2871
phpwork512 <phpwork512@yandex.ru>
2023-01-20 13:48:56 +0400

    sprint 13 add-controllers step#2

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -5,11 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.dto.ItemDtoMapper;
 import ru.practicum.shareit.item.model.Item;
 
 import javax.validation.Valid;
 import javax.validation.ValidationException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -128,5 +128,52 @@ public class ItemController {
         }
 
         itemService.delete(itemId, ownerId);
+    }
+
+    /**
+     * утилитарный класс для преобразования Item <--> ItemDto
+     */
+    private static class ItemDtoMapper {
+        /**
+         * преобразовать ItemDto в Item
+         *
+         * @param itemDto объект ItemDto
+         * @return объект Item или если itemDto был null, то возвращает тоже null
+         */
+        public static Item toItem(ItemDto itemDto) {
+            if (itemDto != null)
+                return new Item(itemDto.getId(), 0, itemDto.getName(), itemDto.getDescription(), itemDto.getAvailable());
+            else return null;
+        }
+
+        /**
+         * преобразовать Item в ItemDto
+         *
+         * @param item объект Item
+         * @return объект ItemDto или если item был null, то возвращает тоже null
+         */
+        public static ItemDto toItemDto(Item item) {
+            if (item != null)
+                return new ItemDto(item.getId(), item.getName(), item.getDescription(), item.getAvailable());
+            else return null;
+        }
+
+        /**
+         * преобразовать список Item в список ItemDto
+         *
+         * @param itemList список объектов Item
+         * @return список объектов ItemDto
+         */
+        public static List<ItemDto> toItemDtoList(List<Item> itemList) {
+            List<ItemDto> itemDtoList = new ArrayList<>();
+
+            if (itemList != null) {
+                for (Item item : itemList) {
+                    itemDtoList.add(toItemDto(item));
+                }
+            }
+
+            return itemDtoList;
+        }
     }
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -4,10 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.exceptions.UserEmailNotUniqueException;
 import ru.practicum.shareit.user.dto.UserDto;
-import ru.practicum.shareit.user.dto.UserDtoMapper;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -95,5 +96,81 @@ public class UserController {
     public void delete(@PathVariable int userId) {
         log.info("Delete userId {}", userId);
         userService.delete(userId);
+    }
+
+    /**
+     * утилитарный класс для дополнительной валидации модели User
+     */
+    private static class UserValidator {
+        /**
+         * дополнительная валидация пользователя: проверка уникальности e-mail
+         *
+         * @param user        объект User
+         * @param userService объект-сервис работы с пользователями
+         * @throws UserEmailNotUniqueException если обнаружено, что e-mail не уникальный
+         */
+        public static void validate(User user, UserService userService) throws UserEmailNotUniqueException {
+            //дополнительная проверка уникальности e-mail среди всех записей в хранилище пользователей
+            boolean isNewUser = user.getId() == 0;
+            String userEmail = user.getEmail();
+
+            //e-mail может быть пустым при обновлении существующего пользователя
+            if (isNewUser || userEmail != null && !userEmail.isBlank()) {
+                List<User> allUsers = userService.getAll();
+                for (User userEntry : allUsers) {
+                    if (userEntry.getEmail().equals(userEmail)) {
+                        if (isNewUser || userEntry.getId() != user.getId()) {
+                            throw new UserEmailNotUniqueException("E-mail не уникален");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * утилитарный класс для преобразования User <--> UserDto
+     */
+    private static class UserDtoMapper {
+
+        /**
+         * преобразовать dto в бизнес-объект User
+         *
+         * @param userDto объект UserDto
+         * @return объект User или null если на входе был null
+         */
+        public static User toUser(UserDto userDto) {
+            if (userDto != null) return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
+            else return null;
+        }
+
+        /**
+         * преобразовать бизнес-объект User в dto
+         *
+         * @param user объект User
+         * @return объект UserDto или null если на входе был null
+         */
+        public static UserDto toUserDto(User user) {
+            if (user != null) return new UserDto(user.getId(), user.getName(), user.getEmail());
+            else return null;
+        }
+
+        /**
+         * преобразовать список бизнес-объектов User в список dto
+         *
+         * @param userList список объектов User
+         * @return список объектов UserDto
+         */
+        public static List<UserDto> toUserDtoList(List<User> userList) {
+            List<UserDto> userDtoList = new ArrayList<>();
+
+            if (userList != null) {
+                for (User user : userList) {
+                    userDtoList.add(toUserDto(user));
+                }
+            }
+
+            return userDtoList;
+        }
     }
 }
